ff86fbcb847a0f9a8cd433255875c8e6881f42ab
Lóránt Pintér <lorant.pinter@gmail.com>
2018-09-14 10:29:19 +0200

    Add Gradle Groovy package version

--- test/src/test/groovy/GroovyTest.groovy
+++ test/src/test/groovy/GroovyTest.groovy
@@ -3,5 +3,13 @@ class GroovyTest {
     void "test can be compiled and executed"() {
         println "Groovy version: ${GroovySystem.version} at " +
             new Date().format("yyyy-MM-dd")
+
+        def gradleGroovyVersionProps = new Properties()
+        gradleGroovyVersionProps.load(
+            new StringReader(
+                getClass().getResource("/gradle-groovy-all-version.properties").text
+            )
+        )
+        println "Gradle Groovy package version: ${gradleGroovyVersionProps.version}"
     }
 }
